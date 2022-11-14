20d8a50af17968a16758626de2684a710b3032a6
amol <amolwaghole43@gmail.com>
2022-11-14 12:17:13 +0530

    Architect Asian development

--- src/main/java/com/Introduction/App.java
+++ src/main/java/com/Introduction/App.java
@@ -9,7 +9,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello to my World!" );
-        System.out.println( "Hello to my World!" );
+
+        System.out.println( "Hello World1!" );
+        System.out.println( "Hello World2!" );
+        
+        System.out.println("Hello country!");
+        System.out.println("Hello Nation!");
+        System.out.println("Hello Culture!");
+        
+        
+        System.out.println("Hello District!");
+        System.out.println("Hello Taluka!");
+        System.out.println("Hello City!");
+
     }
 }
